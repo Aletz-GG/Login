2f1564cd41f8bec960eddf6ff4554af6c33d7e9e
Alexander <ci9395846712023@josepardo.edu.pe>
2025-09-21 17:49:33 -0500

    last-version

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-const logreBox = document.querySelector('logreg-box');
-const loginLink = document.querySelector('login-link');
-const registerLink = document.querySelector('register-link');
+//Obterner elemento activo
 
-registerLink.addEventListener('click', () => {
-    logreBox.classList.add('active');
+//Remover clase activo al elemento obtenido
+
+//Añadir clase activo al elemento seleccionado
+
+const formBoxes = document.querySelectorAll('.form-box');
+const links = document.querySelectorAll('.login-link, .register-link, .password-link, .change-link');
+
+
+function activateForm(target) {
+
+    formBoxes.forEach(form => form.classList.remove('active'));
+    target.classList.add('active');
+}
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        const targetClass = link.classList[0].replace('-link', '');
+        const targetForm = document.querySelector(`.${targetClass}`);
+        activateForm(targetForm);
+    });
 });
